5a7f8bf8e65d14755e52e3316b1579c8b5afe24d
lmaoclost <renansmoliveira@gmail.com>
2020-10-21 17:53:14 -0300

    Ratings Service

--- find_my_coffee_web/src/services/ratings.js
+++ find_my_coffee_web/src/services/ratings.js
@@ -1,7 +1,8 @@
 import api from './api';
-​
+
 const RatingService = {
- create: (store, rating) => api.post('/ratings', { store: store, rating: rating} ),
-}
-​
+  create: (store, rating) =>
+    api.post('/ratings', { store: store, rating: rating }),
+};
+
 export default RatingService;
