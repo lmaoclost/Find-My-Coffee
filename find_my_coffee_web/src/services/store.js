8e9f01841d8bad42b1f453169e085cb86a320135
lmaoclost <renansmoliveira@gmail.com>
2020-10-21 17:53:06 -0300

    Store service

--- find_my_coffee_web/src/services/store.js
+++ find_my_coffee_web/src/services/store.js
@@ -1,8 +1,10 @@
 import api from './api';
-​
+
 const StoreService = {
- show: (google_place_id) => api.get(`/stores/${google_place_id}`),
- index: (latitude, longitude) => api.get('/stores', {params: {latitude: latitude, longitude: longitude}}),
-}
-​
+  show: (google_place_id) => api.get(`/stores/${google_place_id}`),
+  index: (latitude, longitude) =>
+    api.get('/stores', {
+      params: { latitude: latitude, longitude: longitude },
+    }),
+};
 export default StoreService;
