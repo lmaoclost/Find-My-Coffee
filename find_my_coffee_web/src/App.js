35caeb9c12e01233a71191ef634a2bda6559dd93
lmaoclost <renansmoliveira@gmail.com>
2020-10-21 15:26:33 -0300

    Add Google Maps

--- find_my_coffee_web/src/App.js
+++ find_my_coffee_web/src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 function App() {
-  return <>Hello OneBitCode</>;
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  const { REACT_APP_GOOGLE_API_KEY } = process.env;
+
+  useEffect(() => {
+    setCurrentLocation();
+  }, []);
+
+  async function setCurrentLocation() {
+    await navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      (error) => {
+        alert('Habilite a localização para usar esse APP');
+      }
+    );
+  }
+
+  return (
+    <>
+      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={{ height: '100vh', width: '100%' }}
+          zoom={15}
+          center={{ lat: latitude, lng: longitude }}
+        ></GoogleMap>
+      </LoadScript>
+    </>
+  );
 }
 
 export default App;
